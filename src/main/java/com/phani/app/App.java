1a2e1934b637815ea2c1e2902b1f240327acc55c
phanirag <raghavs025@gmail.com>
2020-01-21 18:04:04 +0530

    added new data

--- src/main/java/com/phani/app/App.java
+++ src/main/java/com/phani/app/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
 		System.out.println( "Jenkins exmple" );
-		System.out.println( "Added webhook" );
+		System.out.println( "Added webhookaasdasdas" );
     }
 }
